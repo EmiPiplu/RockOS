4fe508493bf61fbf66d015f5d8af4be890165d11
Emi <emitr2083@gmail.com>
2026-05-10 19:52:53 +0100

    first commit

---
+++ bootloader/src/main.c
@@ -0,0 +1,16 @@
+#include "efi.h"
+
+// Entry Point
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    (void)ImageHandle;   
+
+    SystemTable->ConOut->Reset(SystemTable->ConOut, false);
+
+    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, u"Hello World!\r\n");
+    
+    while (1) ;
+
+    return EFI_SUCCESS;
+}
